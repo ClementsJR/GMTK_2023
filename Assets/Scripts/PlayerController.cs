eb12541e02d77bcbf052fff6afea688acca50658
ClementsJR <jrclements@ualr.edu>
2023-07-07 20:48:43 -0400

    Improved player movement script; created player prefab; created crosshair

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField][Range(0.0f, 2.0f)]
-    float speed = 1.0f;
-    [SerializeField][Range(0.0f, 5000.0f)]
-    float jumpForce = 1000.0f;
+    [SerializeField][Range(0f, 5f)]
+    float lookSpeed = 3f;
+    [SerializeField][Range(0f, 50f)]
+    float moveSpeed = 10f;
+    [SerializeField][Range(0f, 5000f)]
+    float jumpForce = 1000f;
     [SerializeField]
     Rigidbody physicsBody;
 
-    void Update() {
-        Vector3 fwdMovement = Input.GetAxis("Vertical") * speed * transform.forward;
-        Vector3 sideMovement = Input.GetAxis("Horizontal") * speed * transform.right;
+	private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update() {
+        UpdateLook();
+        UpdateMovement();
+        //UpdateJump();
+    }
+
+    void UpdateLook() {
+        float horizontalLook = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up * horizontalLook * lookSpeed);
+
+        Transform camera = Camera.main.transform;
+        float verticalLook = Mathf.Clamp(-Input.GetAxis("Mouse Y"), -15f, 15f);
+        camera.Rotate(camera.right * verticalLook * lookSpeed, Space.World);
+    }
+
+    void UpdateMovement() {
+        Vector3 fwdMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime * Vector3.forward;
+        Vector3 sideMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime * Vector3.right;
         Vector3 movement = fwdMovement + sideMovement;
 
-        transform.Translate(movement);
+        transform.Translate(movement,Space.Self);
+    }
 
-        if(Input.GetButtonDown("Jump")) {
+    void UpdateJump() {
+        if (Input.GetButtonDown("Jump")) {
             physicsBody.AddForce(transform.up * jumpForce);
-		}
+        }
     }
 }
